b4327a386706fa0bd519c855d4a9160cf5bfa9d8
Tyree Nettles <nettles.code@gmail.com>
2021-04-25 23:17:34 -0500

    In process

--- js/scripts.js
+++ js/scripts.js
@@ -1,71 +1,31 @@
 //define variables // 
-
-var startQuiz = document.getElementById('.start');
-var highScore = document.getElementById('.highScore')
-var timerCount = document.getElementById('.timer')
-var mainPage = document.getElementById('.main')
-var clearHighScore = document.getElementById('.clearScore')
-var submitScores = document.getElementById ('.submit')
-
-var questions = ['Strings are wrapped in?', 'Which is not a data type?','The "dot operator" represents?','Const variables may be redefined by updating the defined const variable?','String interpolations can be performed by using "template numerals"?','"Null" and "undefined" may be used interchangably?','How many primative data types are there?','Which will cause a syntax error?'];
-
-
-//funtions//
-function init() {
-
+let introPage = document.getElementById ('intro-page');
+let timerCount = document.getElementById('timer');
+let startQuiz = document.getElementById('start-button');
+let questionPage = document.getElementById ('question-page');
+let questionBank = document.getElementById('questions');
+let multipleChoices = document.getElementById('answers');
+let highScorePage = document.getElementById('final-page');
+let finalScore = document.getElementById('final-score');
+let HighScore = document.getElementById('submit highScore');
+let clearHighScore = document.getElementById('clearScore');
+let submitScores = document.getElementById ('submit');
+let currentQuestion = [0];
 
 
-
-function startQuiz(){
-    timerCount = 10;
-//find dom element to show an area on html and show the first question there
-getQuestion();
-startTimer()
-}
-}
+//functions//
 
 //get the next question
-function getQuestion(){
-// get the current question
-//show the questiontt
-//loop show the choices (buttons)
-//add enent listener for each button created 
-answerCheck();
+function getQuestion() {
+    // get the current question
+    if (currentQuestion < questions.length) {
+        questionPage.classList.remove('hide');
+        introPage.classList.add('hide');
+        questionBank.innerHTML = questions[currentQuestion].question;
+        
+    } else {
+        endGame();
+    }
 }
 
-//check user selection
-function answerCheck(){
-    //check the user selection against the correct answer
-    //incorrect-removes seconds
-    //set score
-    getQuestion();
-    //if questions.length
-    endGame();
-}
 
-//end game
-function endgame(){
-    //set their scores
-    //clear out timer
-    //show end screen
-}
-
-//save high score
-function saveHighScore(){
-    //prompt for initials
-    //save score to localstorage
-
-}
-
-//all event listeners//
-//1. start button//
-startButton.addEventListener('click', startQuiz);
-
-// save high score
-saveScore.addEventListener('click', saveHighScore)
-
-
-
-
-// Calls init() so that it fires when page opened
-init();
